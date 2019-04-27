bcf8a5861dca92b87e809ed53ee3b1a46be85209
Paulo-Sergio <paulofranca.contato@gmail.com>
2019-04-27 19:00:28 -0300

    habilitando cors na aplicação

--- src/main/java/br/com/restful/udemy/config/WebConfig.java
+++ src/main/java/br/com/restful/udemy/config/WebConfig.java
@@ -3,11 +3,20 @@ package br.com.restful.udemy.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 //@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+	
+	/**
+	 * habilitando CORS globalmente na aplicação
+	 */
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+	}
 
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
